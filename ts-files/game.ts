6a52dcf989b79ac4cc84bb31ae4a2b637ebb0813
lywonglokyin <lywongah@connect.ust.hk>
2021-04-07 00:27:48 +0800

    basic gameloop structure.

--- ts-files/game.ts
+++ ts-files/game.ts
@@ -1,57 +1,35 @@
 import * as pixi_namespace from 'pixi.js'
-import { Application, Rectangle, Sprite } from 'pixi.js';
+import { Application, PlaneGeometry, Rectangle, Sprite } from 'pixi.js';
 import { movableSprite } from './moveableSprite';
 
 declare var PIXI:  typeof pixi_namespace;
 
-const app: Application = new PIXI.Application({transparent: true});
+const app: Application = new PIXI.Application({transparent: false});
 document.body.appendChild(app.view);
 
+let game_state: string = "play";
 
+let commander: movableSprite = PIXI.Sprite.from('images/commander.png');
+commander.anchor.set(0.5);
 
-let cats: movableSprite[] = [];
-let total_cats: number = 50;
+commander.x = 100;
+commander.y = 100;
 
-for (let i = 0; i < total_cats; i++){
-    const cat: movableSprite = PIXI.Sprite.from('images/cat.png');
-    cat.anchor.set(0.5);
+app.stage.addChild(commander);
 
-    cat.x = Math.random() * app.screen.width;
-    cat.y = Math.random() * app.screen.height;
+app.ticker.add(()=>{
+    gameLoop();
+})
 
-    cat.tint = Math.random() * 0xFFFF;
-
-    cat.direction = Math.random() * Math.PI * 2;
-    cat.turningSpeed = Math.random() - 0.8;
-    cat.speed = 2 + Math.random()*2;
-
-    cats.push(cat);
-
-    app.stage.addChild(cat);
+function gameLoop(){
+    if (game_state === "play"){
+        playFrame();
+    }
 }
 
-let catBoundsPadding: number = 100;
-let catBounds : Rectangle = new PIXI.Rectangle(-catBoundsPadding, -catBoundsPadding,
-    app.screen.width + catBoundsPadding * 2, app.screen.height + catBoundsPadding * 2);
-
-app.ticker.add(function(){
-    for (let i = 0; i < cats.length; i++) {
-        let cat = cats[i];
-        cat.direction! += cat.turningSpeed! * 0.01;
-        cat.x += Math.sin(cat.direction!) * cat.speed!;
-        cat.y += Math.cos(cat.direction!) * cat.speed!;
-        cat.rotation! = -cat.direction! - Math.PI / 2;
-
-        if (cat.x < catBounds.x) {
-            cat.x += catBounds.width;
-        } else if (cat.x > catBounds.x + catBounds.width) {
-            cat.x -= catBounds.width;
-        }
-
-        if (cat.y < catBounds.y) {
-            cat.y += catBounds.height;
-        } else if (cat.y > catBounds.y + catBounds.height) {
-            cat.y -= catBounds.height;
-        }
+function playFrame(){
+    commander.x += 1;
+    if (commander.x == 200){
+        commander.x = 10
     }
-})
+}
